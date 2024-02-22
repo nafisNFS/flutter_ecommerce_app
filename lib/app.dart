2e9644933e7775610d257803a991eb52480e7ff9
Nafis Ahmed <136017530+nafisNFS@users.noreply.github.com>
2024-02-22 13:27:48 +0600

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/theme/theme.dart';
+import 'package:get/get.dart';
+import 'package:t_store/features/authentication/screens.onboarding/onboarding.dart';
+
+import 'features/authentication/screens.onboarding/onboarding.dart';
+
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: TTexts.appName,
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      // initialBinding: GeneralBindings(),
+      home: const OnBoardingScreen(),
+    );
+  }
+}
