bb4eb0bac77adc93ef9e00ab331043a82596402d
Rubaid <rubaids0911@gmail.com>
2024-02-23 12:46:11 +0600

    forgot password added

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/authentication/screens/password_configuration/forget_password.dart';
+import 'package:t_store/features/authentication/screens/signup/signup.dart';
 
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/constants/text_strings.dart';
-import '../../signup/signup.dart';
 
 class TLoginForm extends StatelessWidget {
   const TLoginForm({
@@ -13,31 +15,20 @@ class TLoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-        child: Padding(
+    return Form(child: Padding(
       padding: const EdgeInsets.symmetric(vertical: TSizes.spaceBtwSections),
       child: Column(
         children: [
           ///Email
           TextFormField(
-            decoration: const InputDecoration(
-              prefixIcon: Icon(Iconsax.direct_right),
-              labelText: TTexts.email,
-            ),
+            decoration:  const InputDecoration(prefixIcon: Icon(Iconsax.direct_right), labelText: TTexts.email,),
           ),
-          const SizedBox(
-            height: TSizes.spaceBtwInputFields,
-          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields,),
           //Password
           TextFormField(
-            decoration: const InputDecoration(
-                prefixIcon: Icon(Iconsax.password_check),
-                labelText: TTexts.password,
-                suffixIcon: Icon(Iconsax.eye_slash)),
+            decoration:  const InputDecoration(prefixIcon: Icon(Iconsax.password_check), labelText: TTexts.password,suffixIcon:Icon(Iconsax.eye_slash) ),
           ),
-          const SizedBox(
-            height: TSizes.spaceBtwInputFields / 2,
-          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields / 2,),
 
           ///Remember me &Forget password
           Row(
@@ -46,33 +37,23 @@ class TLoginForm extends StatelessWidget {
               ///Remember me
               Row(
                 children: [
-                  Checkbox(value: true, onChanged: (value) {}),
+                  Checkbox(value: true, onChanged: (value){}),
                   const Text(TTexts.rememberMe),
                 ],
               ),
-
               ///Forget Password
-              TextButton(
-                  onPressed: () {}, child: const Text(TTexts.forgetPassword)),
+              TextButton(onPressed: () => Get.to(() => const ForgetPassword()), child: const Text(TTexts.forgetPassword)),
             ],
           ),
           const SizedBox(height: TSizes.spaceBtwSections),
-
           ///Sign in button
-          SizedBox(
-              width: double.infinity,
-              child:
-                  ElevatedButton(onPressed: () {}, child: Text(TTexts.signIn))),
+          SizedBox(width: double.infinity,child: ElevatedButton(onPressed: (){}, child: Text(TTexts.signIn))),
           const SizedBox(height: TSizes.spaceBtwItems),
-
           ///Create account button
-          SizedBox(
-              width: double.infinity,
-              child: OutlinedButton(
-                  onPressed: () => Get.to(()=>const SignupScreen()),
-                  child: Text(TTexts.createAccount))),
+          SizedBox(width: double.infinity,child: OutlinedButton(onPressed: () => Get.to(() => const SignupScreen()), child: Text(TTexts.createAccount))),
         ],
       ),
-    ));
+    )
+    );
   }
 }
