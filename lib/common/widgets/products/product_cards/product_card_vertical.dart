3ea7c120295a29fb8cba215d832ec91222243aaa
Rubaid <rubaids0911@gmail.com>
2024-02-26 18:25:20 +0600

    product details added

--- lib/common/widgets/products/product_cards/product_card_vertical.dart
+++ lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/shop/screens/product_details/product_detail.dart';
 
 import '../../../../utils/constants/colors.dart';
 import '../../../../utils/constants/image_strings.dart';
@@ -21,7 +23,7 @@ class TProductCardVertical extends StatelessWidget {
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
     return GestureDetector(
-      onTap: () {},
+      onTap: () => Get.to(() => const ProductDetailScreen()),
       child: Container(
         width: 180,
         padding: const EdgeInsets.all(1),
---
+++ lib/features/shop/screens/product_details/widgets/rating_share_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:t_store/common/widgets/icons/t_circular_icon.dart';
+import 'package:t_store/common/widgets/images/t_rounded_image.dart';
+import 'package:t_store/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+
+
+class TRatingAndShare extends StatelessWidget {
+  const TRatingAndShare({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        /// Rating
+        Row(
+          children: [
+            const Icon(Iconsax.star5, color: Colors.amber, size: 24),
+            const SizedBox(width: TSizes.spaceBtwItems / 2),
+            Text.rich(
+                TextSpan(
+                    children: [
+                      TextSpan(text: '5.0', style: Theme.of(context).textTheme.bodyLarge),
+                      const TextSpan(text: '(199)'),
+                    ]
+                )
+            )
+          ],
+        ),
+        /// Share Button
+        IconButton(onPressed: () {}, icon: const Icon(Icons.share, size: TSizes.iconMd))
+      ],
+    );
+  }
+}
