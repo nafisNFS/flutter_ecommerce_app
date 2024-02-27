4a9895cff0bbee1aca6d4189c42009e913d17d60
Rubaid <rubaids0911@gmail.com>
2024-02-27 16:35:03 +0600

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:t_store/app.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+          (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
+
   runApp(const App());
 }
