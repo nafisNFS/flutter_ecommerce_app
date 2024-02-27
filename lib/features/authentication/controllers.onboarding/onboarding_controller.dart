a76327d95f190a86a5f08c600bbd0047d012745f
Rubaid <rubaids0911@gmail.com>
2024-02-27 17:52:49 +0600

    onboarding control done

--- lib/features/authentication/controllers.onboarding/onboarding_controller.dart
+++ lib/features/authentication/controllers.onboarding/onboarding_controller.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import '../screens/login/login.dart';
 
@@ -16,6 +18,14 @@ class OnBoardingController extends GetxController{
 
   void nextPage(){
     if(currentPageIndex.value==2){
+      final storage = GetStorage();
+
+      if (kDebugMode) {
+        print('========= GET STORAGE Next Button =========');
+        print(storage.read('IsFirstTime'));
+      }
+
+      storage.write('IsFirstTime', false);
       Get.offAll(const LoginScreen());
     }
     else{
