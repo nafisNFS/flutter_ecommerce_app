2945443603d5564aee3110b4498b00b422612f3a
Rubaid <rubaids0911@gmail.com>
2024-03-06 15:14:58 +0600

    Address update done

--- lib/features/personalization/screens/address/address.dart
+++ lib/features/personalization/screens/address/address.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/features/personalization/controllers/address_controller.dart';
 import 'package:t_store/features/personalization/screens/address/add_new_address.dart';
 import 'package:t_store/features/personalization/screens/address/widgets/single_address.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/helpers/cloud_helper_functions.dart';
 
 class UserAddressScreen extends StatelessWidget {
   const UserAddressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(AddressController());
+
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         backgroundColor: TColors.primary,
@@ -24,14 +28,31 @@ class UserAddressScreen extends StatelessWidget {
         title: Text('Addresses', style: Theme.of(context).textTheme.headlineSmall,),
       ),
 
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(TSizes.defaultSpace),
-          child: Column(
-            children: [
-              TSingleAddress(selectedAddress: true),
-              TSingleAddress(selectedAddress: false)
-            ],
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Obx(
+              () => FutureBuilder(
+                // Use key to trigger refresh
+              key: Key(controller.refreshData.value.toString()),
+                future: controller.getAllUserAddresses(),
+                builder: (context, snapshot) {
+
+                  /// Helper function: Handle Loader, No Record, OR ERROR Message
+                  final response = TCloudHelperFunctions.checkMultiRecordState(snapshot: snapshot);
+                  if (response != null) return response;
+
+                  final addresses = snapshot.data!;
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: addresses.length,
+                    itemBuilder: (_, index) => TSingleAddress(
+                      address: addresses[index],
+                      onTap: () => controller.selectAddress(addresses[index]),
+                    ),
+                  );
+                }
+            ),
           ),
         ),
       ),
