139d040195daa8c2eab744df03f6c2bdf9efb2c7
Rubaid <rubaids0911@gmail.com>
2024-03-07 13:44:12 +0600

    Categories backend added

--- lib/common/widgets/image_text_widgets/vertical_image_text.dart
+++ lib/common/widgets/image_text_widgets/vertical_image_text.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/images/t_circular_image.dart';
+import 'package:t_store/features/shop/models/category_model.dart';
 
 import '../../../utils/constants/colors.dart';
 import '../../../utils/constants/sizes.dart';
@@ -6,18 +8,21 @@ import '../../../utils/helpers/helper_functions.dart';
 
 
 class TVerticalImageText extends StatelessWidget {
-  final String image, title;
-  final Color textColor;
-  final Color? backgroundColor;
-  final void Function()? onTap;
   const TVerticalImageText({
     super.key,
     required this.image,
     required this.title,
     this.textColor = TColors.white,
     this.backgroundColor = TColors.white,
+    this.isNetworkImage = true,
     this.onTap,
   });
+
+  final String image, title;
+  final Color textColor;
+  final Color? backgroundColor;
+  final bool isNetworkImage;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
@@ -26,40 +31,44 @@ class TVerticalImageText extends StatelessWidget {
       onTap: onTap,
       child: Padding(
         padding: const EdgeInsets.only(right: TSizes.spaceBtwItems),
-        child: Column(
-          children: [
-            //circular icon
-            Container(
-              width: 56,
-              height: 56,
-              padding: const EdgeInsets.all(TSizes.sm),
-              decoration: BoxDecoration(
-                color: backgroundColor ?? (dark ? TColors.black : TColors.white),
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: Center(
-                child: Image(
-                  image: AssetImage(image),
-                  fit: BoxFit.cover,
-                  color: TColors.dark,
+          child: Column(
+            children: [
+              //circular icon
+              Container(
+                width: 56,
+                height: 56,
+                padding: const EdgeInsets.all(TSizes.sm),
+                decoration: BoxDecoration(
+                  color: backgroundColor ?? (dark ? TColors.black : TColors.white),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: Center(
+                  child: TCircularImage(
+                    image: image,
+                    fit: BoxFit.fitWidth,
+                    padding: TSizes.sm * 1.4,
+                    isNetworkImage: isNetworkImage,
+                    backgroundColor: backgroundColor,
+                    overlayColor: THelperFunctions.isDarkMode(context) ? TColors.light : TColors.dark,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: TSizes.spaceBtwItems / 2),
-
-            //text
-            SizedBox(
-              width: 55,
-              child: Text(
-                title,
-                style: Theme.of(context).textTheme.labelMedium!.apply(color: textColor),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-            )
-          ],
+              const SizedBox(height: TSizes.spaceBtwItems / 2),
+          
+              //text
+              SizedBox(
+                width: 55,
+                child: Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.labelMedium!.apply(color: textColor),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              )
+            ],
+          ),
         ),
-      ),
     );
   }
 }
