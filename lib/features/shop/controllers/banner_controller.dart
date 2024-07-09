a0037562c966641a5069299634fa7582f67e06b8
Rubaid <rubaids0911@gmail.com>
2024-07-09 10:44:06 +0600

    Recipe Details fetch done

--- lib/features/shop/controllers/banner_controller.dart
+++ lib/features/shop/controllers/banner_controller.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+// import 'dart:ffi';
 
 import 'package:get/get.dart';
 import 'package:t_store/common/widgets/loader/loaders.dart';
--- lib/features/shop/screens/order/widgets/orders_list.dart
+++ lib/features/shop/screens/order/widgets/orders_list.dart
@@ -14,7 +14,7 @@ class TOrderListItems extends StatelessWidget {
     final dark = THelperFunctions.isDarkMode(context);
     return ListView.separated(
       shrinkWrap: true,
-      itemCount: 7,
+      itemCount: 1,
       separatorBuilder: (_, __) => const SizedBox(
         height: TSizes.spaceBtwItems,
       ),
@@ -43,7 +43,7 @@ class TOrderListItems extends StatelessWidget {
                             .apply(color: TColors.primary, fontWeightDelta: 1),
                       ),
                       Text(
-                        '08 Nov 2023',
+                        '10 Mar 2024',
                         style: Theme.of(context).textTheme.headlineSmall,
                       )
                     ],
@@ -103,7 +103,7 @@ class TOrderListItems extends StatelessWidget {
                               style: Theme.of(context).textTheme.labelMedium,
                             ),
                             Text(
-                              '14 April 2023',
+                              '12 March 2024',
                               style: Theme.of(context).textTheme.titleMedium,
                             )
                           ],
